73600d3a03a1b231ead6cec2c2daef342e0c9e25
Hussein Beygi <hussein1997.hb@hotmail.com>
2023-06-23 18:43:38 +0330

    Add project files.

---
+++ 34 - ReportProgressTask/ReportProgressTask/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ReportProgressTask
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var progress = new Progress<double>();
+			progress.ProgressChanged += (sender, args) =>
+			{
+				Console.WriteLine(value: $"Progress : {args}");
+			};
+
+
+			await MyMethodAsync(progress);
+		}
+
+		static async Task MyMethodAsync(IProgress<double> progress = null)
+		{
+			bool done = false;
+			double percentComplete = 0;
+			while (!done)
+			{
+
+				await Task.Delay(millisecondsDelay: 100);
+
+				percentComplete++;
+
+				progress?.Report(value: percentComplete);
+
+				if (percentComplete > 99)
+					done = true;
+
+			}
+		}
+	}
+}
